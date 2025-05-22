9d7b9aa5f8ab3b139612044effaa7d3ef3f9351b
VictorLCosta <victorlc2019@outlook.com>
2025-05-22 12:20:06 -0300

    feat: enhance search functionality with pagination and filtering options

--- src/SearchService/Endpoints/SearchEndpoints.cs
+++ src/SearchService/Endpoints/SearchEndpoints.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using SearchService.Entities;
+using SearchService.RequestHelpers;
 
 namespace SearchService.Endpoints;
 
@@ -12,16 +14,44 @@ public static class SearchEndpoints
         group.MapGet("/", SearchItemsAsync);
     }
 
-    private static async Task<Ok<List<Item>>> SearchItemsAsync(string searchTerm)
+    private static async Task<Ok<List<Item>>> SearchItemsAsync([FromQuery] SearchParams searchParams)
     {
         var query = DB
             .Find<Item>()
             .Sort(x => x.Ascending(a => a.Make));
 
-        if (!string.IsNullOrWhiteSpace(searchTerm))
+        if (!string.IsNullOrWhiteSpace(searchParams.SearchTerm))
         {
-            query.Match(Search.Full, searchTerm).SortByTextScore();
+            query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
         }
+
+        query = searchParams.OrderBy switch
+        {
+            "make" => query.Sort(x => x.Descending(a => a.Make)),
+            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd))
+        };
+        
+        query = searchParams.FilterBy switch
+        {
+            "active" => query.Match(x => x.AuctionEnd > DateTime.UtcNow),
+            "ended" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+            _ => query
+        };
+
+        if (!string.IsNullOrEmpty(searchParams.Seller))
+        {
+            query.Match(x => x.Seller == searchParams.Seller);
+        }
+
+        if (!string.IsNullOrEmpty(searchParams.Winner)) 
+        {
+            query.Match(x => x.Winner == searchParams.Winner);
+        }
+
+        query.Skip((searchParams.PageNumber - 1) * searchParams.PageSize)
+            .Limit(searchParams.PageSize);
 
         var result = await query.ExecuteAsync();
 

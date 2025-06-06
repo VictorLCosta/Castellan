4c3c27c500226695ff4fa7da20d26d168a510fad
VictorLCosta <victorlc2019@outlook.com>
2025-06-06 12:06:41 -0300

    feat: complete Client configuration in IdentityService for Next.js App

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -20,7 +20,19 @@ public static class Config
         [
             new Client
             {
-                
+                ClientId = "nextApp",
+                ClientName = "Next.js App",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+                AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+                RequirePkce = false,
+                RedirectUris = { "http://localhost:3000/api/auth/callback/id-server" },
+                AllowOfflineAccess = true,
+                AllowedScopes = {
+                    "openid",
+                    "profile",
+                    "auctionApp"
+                },
+                AccessTokenLifetime = 3600*24*30
             }
         ];
 }
